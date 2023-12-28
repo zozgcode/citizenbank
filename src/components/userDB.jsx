d11d2e5469e8b7f0a2efcb8a91d4f908aa76a6ff
sunshine-web-coder <webdev4gaming@gmail.com>
2023-12-28 15:23:11 +0100

    first commit

---
+++ src/components/userDB.jsx
@@ -0,0 +1,30 @@
+const users = [
+  { 
+    id: 1,
+    name: "Trace Adkins",
+    cardName: "Trace Adkins",
+    username: "traceA125",
+    password: "jjd1!@9ffmsd",
+    amount: "30,000,000.00",
+  },
+  { 
+    id: 2,
+    name: "Anne Marie",
+    cardName: "Anne Marie ",
+    username: "Thegreenherself",
+    password: "Johnwick4",
+    amount: "908,333.45",
+  },
+  { 
+    id: 3,
+    name: "Edward Jameson",
+    cardName: "Edward Jameson",
+    username: "edwardjameson",
+    password: "83er9cm3498",
+    amount: "35,000,000.00",
+  },
+];
+export default users;
+
+
+
